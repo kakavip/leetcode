cbf8a4ed6bf408caa95a695c546c46aada272d88
quangnd <quang.ngo@youthdev.net>
2023-12-18 13:52:29 +0700

    fix: update problem 451

--- python/451.sort-characters-by-frequency.py
+++ python/451.sort-characters-by-frequency.py
@@ -11,23 +11,17 @@ from typing import Dict, List
 
 class Solution:
     def frequencySort(self, s: str) -> str:
+        unique_chars = []
         w_fre_map: Dict[str, int] = {}
         for c in s:
             if c not in w_fre_map:
                 w_fre_map[c] = 0
+                unique_chars.append(c)
             w_fre_map[c] += 1
 
-        fre_map: Dict[int, List[str]] = {}
-        for c in w_fre_map.keys():
-            if w_fre_map[c] not in fre_map:
-                fre_map[w_fre_map[c]] = []
-
-            fre_map[w_fre_map[c]].append(c)
-
         result: str = ""
-        for fre in sorted(list(fre_map.keys()), key=lambda x: -x):
-            for c in fre_map[fre]:
-                result += c * fre
+        for c in sorted(unique_chars, key=lambda x: -w_fre_map[x]):
+            result += c * w_fre_map[c]
 
         return result
 
